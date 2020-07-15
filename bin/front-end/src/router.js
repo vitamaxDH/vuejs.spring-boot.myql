28b90c54eb2f125970ae41026280b02bdd19206e
vitamaxDH <hellonewtry@gmail.com>
2020-07-15 19:35:53 +0900

    연습용

--- bin/front-end/src/router.js
+++ bin/front-end/src/router.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 import LoginPage from '@/views/LoginPage'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
+export default new router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [{
