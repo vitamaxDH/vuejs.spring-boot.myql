09774bdc8291836aa34afe189f229ee1403b0d5d
vitamaxDH <hellonewtry@gmail.com>
2020-07-26 19:35:18 +0900

    수정

--- front-end/tests/unit/LoginPage.spec.js
+++ front-end/tests/unit/LoginPage.spec.js
@@ -2,11 +2,10 @@ import Vue from 'vue'
 import LoginPage from '@/views/LoginPage'
 
 describe('LoginPage.vue', () => {
-    it('should render correct contents', () => {
-        const Constructor = Vue.extend(LoginPage)
-        const vm = new Constructor().$mount()
-        expect(vm.$el.querySelector('h1').textContent)
-            .toEqual('TaskAgile');
-
-    })
+  it('should render correct contents', () => {
+    const Contructor = Vue.extend(LoginPage)
+    const vm = new Contructor().$mount()
+    expect(vm.$el.querySelector('h1').textContent)
+    .toEqual('TaskAgile')
+  }, 10000)
 })
