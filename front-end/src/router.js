738e72811115d686e2ff0a77de828234a60aa6c3
vitamaxDH <hellonewtry@gmail.com>
2020-07-26 21:47:24 +0900

    implement UI of register page

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -1,17 +1,19 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import LoginPage from '@/views/LoginPage'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import LoginPage from "@/views/LoginPage";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes :[{
-    path : '/login',
-    name : 'LoginPage',
-    component : LoginPage
-  }]
-})
+  routes: [
+    {
+      path: "/login",
+      name: "LoginPage",
+      component: LoginPage
+    }
+  ]
+});
 
-export default router
+export default router;
---
+++ front-end/src/views/RegisterPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="register-form">
+        <div class="logo-wrapper"></div>
+        <form action="">
+          <div class="form-group"></div>
+          <div class="form-group"></div>
+          <div class="form-group"></div>
+          <button type="submit" class="btn btn-primary-btn-block"></button>
+        </form>
+      </div>
+    </div>
+    <footer class="footer">
+      <span class="copyright"></span>
+      <ul class="footer-links list-inline float-right"></ul>
+    </footer>
+  </div>
+</template>
+<style lang="scss" scoped>
+.container {
+  max-width: 900px;
+}
+.register-form {
+  margin-top: 50px;
+  max-width: 320px;
+}
+.logo-wrapper {
+  margin-bottom: 40px;
+}
+.footer {
+  width: 100%;
+  line-height: 40px;
+  margin-top: 50px;
+}
+</style>
