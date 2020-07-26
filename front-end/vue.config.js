b8e8a0b8e627a0435f2d3e25527b0ba5582a6a20
vitamaxDH <hellonewtry@gmail.com>
2020-07-26 20:01:58 +0900

    bridge the communication

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     devServer : {
-        port : 3000
+        port : 3000,
+
+        proxy : {
+            '/api/*': {
+                target : 'http://localhost:8080'
+            }
+        }
     }
 }
